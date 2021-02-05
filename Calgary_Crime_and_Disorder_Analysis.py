df3767dd1e946ac99a277646f3754de8deb603d5
Fungai Mawoyo <fungaimawoyo@gmail.com>
2021-02-04 20:16:54 -0700

    added pdf version of report

--- Calgary_Crime_and_Disorder_Analysis.py
+++ Calgary_Crime_and_Disorder_Analysis.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 import seaborn as sns
 sns.set()
 
